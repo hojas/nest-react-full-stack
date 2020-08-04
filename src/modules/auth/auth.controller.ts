f9482596273eb0ce9549035fa0b98543e8e1b681
Yuanxiang Chen <cyxhuan9@163.com>
2020-08-04 14:21:24 +0800

    add api /auth/user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,24 +1,51 @@
 import { Response } from 'express';
-import { Controller, Get, UseGuards, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Req,
+  Res,
+  UseFilters,
+  NotFoundException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthReq } from 'src/types/auth-req';
 import { GithubAuthGuard } from './guards/github-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
+import { User } from '../user/user.entity';
+import { UserService } from '../user/user.service';
 
-@Controller('auth/github')
-@UseGuards(GithubAuthGuard)
+@Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
-  @Get()
+  @Get('github')
+  @UseGuards(GithubAuthGuard)
   auth(): string {
     return '正在登录...';
   }
 
-  @Get('callback')
+  @Get('github/callback')
+  @UseGuards(GithubAuthGuard)
   async callback(@Req() req: AuthReq, @Res() res: Response): Promise<any> {
     const token = await this.authService.validate(req);
     this.authService.setToken(res, token.token);
 
     return res.json({ ok: true });
+  }
+
+  @Get('user')
+  @UseGuards(JwtAuthGuard)
+  @UseFilters(HttpExceptionFilter)
+  async findAuthUser(@Req() req: AuthReq): Promise<User> {
+    if (req.user && req.user.id) {
+      return this.userService.findById(req.user.id);
+    }
+
+    throw new NotFoundException();
   }
 }
